8fe2b256f7125dc088b64ca1b84c7bf7e85fadf5
ol1xy <aleksejserduk1@gmail.com>
2026-02-05 14:58:49 +0800

    Fixed missing tab in if-condition

--- script.py
+++ script.py
@@ -5,5 +5,5 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Denominator couldn't be a zero)
     if isinstance(a, str) or isinstance(b, str):
-	raise ValueError("Divisors couldn't be a string)
+	    raise ValueError("Divisors couldn't be a string)
     return a / b
