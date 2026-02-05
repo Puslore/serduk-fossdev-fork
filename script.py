93d50ba3f4b29851172d5c8aa196d591e83e7fc4
ol1xy <aleksejserduk1@gmail.com>
2026-02-05 14:52:43 +0800

    Added a string-divisors check

--- script.py
+++ script.py
@@ -4,4 +4,6 @@ def sum(a, b):
 def divide(a, b):
     if b == 0:
         raise ValueError("Denominator couldn't be a zero)
+    if isinstance(a, str) or isinstance(b, str):
+	raise ValueError("Divisors couldn't be a string)
     return a / b
