990e22fd8d2c88f3fcdf8395eddfdcf25ef42184
ol1xy <aleksejserduk1@gmail.com>
2026-02-10 14:30:52 +0800

    Added string-division test

--- script.py
+++ script.py
@@ -6,8 +6,4 @@ def divide(a, b):
         raise ValueError("Denominator couldn't be a zero")
     if isinstance(a, str) or isinstance(b, str):
 	    raise ValueError("Divisors couldn't be a string")
-<<<<<<< HEAD
     return a / b
-=======
-    return a / b
->>>>>>> fix/missing-test
--- test.py
+++ test.py
@@ -12,9 +12,16 @@ def test_divide():
     result = 0.5
     assert divide(a, b) == result
 
+def test_divide_prohibited():
+    try:
+        divide("A", "B")
+        assert False
+    except ValueError as e:
+        print('Test string-division fails')
 #def test_divide_zero():
 #need to end it
 
 if __name__ == '__main__':
     test_divide()
     test_sum()
+    test_divide_prohibited()
