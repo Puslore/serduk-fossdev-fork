07440c7128d7084dd04319cf316dd05c0743c521
ol1xy <aleksejserduk1@gmail.com>
2026-02-10 14:38:22 +0800

    Added list division check

--- script.py
+++ script.py
@@ -6,8 +6,6 @@ def divide(a, b):
         raise ValueError("Denominator couldn't be a zero")
     if isinstance(a, str) or isinstance(b, str):
 	    raise ValueError("Divisors couldn't be a string")
-<<<<<<< HEAD
+    if isinstance(a, list) or isinstance(b, list):
+        raise ValueError("Could not divide lists")
     return a / b
-=======
-    return a / b
->>>>>>> fix/missing-test
