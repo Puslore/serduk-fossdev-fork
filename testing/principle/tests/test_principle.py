23c40b934f17d015a5fa49423ac1cf4d38a08028
ol1xy <aleksejserduk1@gmail.com>
2026-03-05 13:04:39 +0800

    Added bad example of test, that uses func operation and depends from it

--- testing/principle/tests/test_principle.py
+++ testing/principle/tests/test_principle.py
@@ -27,6 +27,11 @@ def test_addition_with_bug():
     # found data that make test reliable
     # assert add_with_bug(7, 6) == 13
 
+def test_addition_duplicate():
+    assert add(6, 7) == 6 + 7
+    print("Test DUPLICATE ADDITION PASSED")
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicate()
