7f8ca445f5bda3b4a28f66e9932406c6726ebe29
ol1xy <aleksejserduk1@gmail.com>
2026-03-05 12:58:41 +0800

    Added test for bugged add function, found the reliable test for it, removed extra .gitignore

--- testing/principle/tests/test_principle.py
+++ testing/principle/tests/test_principle.py
@@ -1,13 +1,32 @@
 # import sys
 # sys.path.append("../src")
 
-# TODO make it with `pip install -e math_demo``
+# TODO make it with `pip install -e .`
+# Раннее тестирование позволяет сэкономить время позднее
+# Тесты показывают наличие ошибок, а не отсутсвие
+# Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать новые ошибки (pescicide paradox)
+# Тесты покрывают как успешные, так и ошибочные кейсы
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
     print("Test ADDITION PASSED")
+
+def test_addition_with_bug():
+    # Тесты показывают наличие ошибок, а не их отсутствие
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print("Test BUGGED ADDITION PASSED")
+    # found data that make test reliable
+    # assert add_with_bug(7, 6) == 13
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
