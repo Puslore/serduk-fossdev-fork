dc91b34c82c4e99ccc6a8bdaec2f509124d89927
ol1xy <aleksejserduk1@gmail.com>
2026-03-05 13:47:44 +0800

    Fixed test issue

--- testing/principle/tests/test_principle.py
+++ testing/principle/tests/test_principle.py
@@ -7,6 +7,8 @@
 # Тесты не должны дублировать логику тестируемого кода
 # Тесты не должны использовать ВСЕ наборы входных параметров
 # Тесты должны покрывать "кластеры" входных параметров
+# [DONE] Тестовые функции должны тестировать логические блоки
+
 # Тесты должны обнаруживать новые ошибки (pescicide paradox)
 # Тесты покрывают как успешные, так и ошибочные кейсы
 
@@ -47,13 +49,11 @@ def test_addition_clusters():
     assert add(-10, -11) == -21
     assert add(-5, 0) == -5
     assert add(0, -2) == -2
-    assert(9, 5) == 14
-    assert(5, 9) == 14
     print("Test CLUSTERS PASSED")
 
 def test_addition_commutative():
-    assert(9, 5) == 14
-    assert(5, 9) == 14  
+    assert add(9, 5) == 14
+    assert add(5, 9) == 14  
     print("Test COMMUTATIVE PASSED")
 
 if __name__ == "__main__":
@@ -62,3 +62,4 @@ if __name__ == "__main__":
     test_addition_duplicate()
     # test_addiction_overkill() #try it on your risk
     test_addition_clusters()
+    test_addition_commutative()
